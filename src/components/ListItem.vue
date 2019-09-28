4489dd4d3c1028863d9db26d0d7ec4825e0a843d
Fenja13 <feni.goetz@gmail.com>
2019-09-28 19:02:09 +0200

    Listen komponente hinzugefügt

---
+++ src/components/ListItem.vue
@@ -0,0 +1,45 @@
+<template>
+<li>
+    <div class="Datum"> 20.09.2019 </div>
+    <h2><span class="Gruppenname"> Fridays for Future </span>Demo</h2>
+    <ul>
+        <li> <strong>Treffpunkt</strong> Alte Oper</li>
+        <li> <strong>Start</strong> 12 Uhr</li>
+        <li> <strong>Ziel</strong> Römerplatz</li>
+        <li> <strong>Strecke</strong> 3km</li>
+    </ul>
+</li>
+    
+</template>
+
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style>
+    ul {
+        padding-left: 0px;
+        list-style-type: none;
+    }
+    .Datum {
+        font-weight:bold;
+        color:black
+    }
+    .Gruppenname {
+        color:darkgreen;
+        font-size: 20pt;
+    }
+    strong {
+        text-align: left
+    }
+    h2 {
+        margin-top: 0;
+    }
+</style>
