dc6d3841a99d2b69d4180d59d3f6033406e97772
Moritz Schulz <moritzleni@gmail.com>
2019-09-29 10:46:40 +0200

    Designed Searchbox

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="inline">
+	<input type="text" v-model="searchterm" placeholder="Stadt"> </input>
+	<button>🔍</button>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'SearchBox',
+  data() { return {
+    show: false
+  }},
+  methods: {
+    loadinfo: function() {
+      this.show = true;
+    }
+  },
+  props: {
+    date: String,
+    event: String,
+    treffpunkt: String,
+    time: String,
+    ziel: String,
+    strecke: String,
+    titel: String,
+    beschreibung: String
+  }
+}
+</script>
+
+
+<style scoped>
+.inline * {
+    display: inline-block;
+    max-height: 55px;
+}
+.inline {
+    margin: 1.8em auto 1.2em auto;
+    display: flex;
+    justify-content: center;
+}
+button {
+    background: white;
+    border-radius: 0px 8px 8px 0px;
+    border: none;
+    cursor: pointer;
+    font-size: 22pt;
+    vertical-align: center;
+    line-height: 1.8em;
+}
+input {
+    font-size: 18pt;
+    padding: 4px 12px;
+    border-radius: 8px 0px 0px 8px;
+    border: none;
+}
+
+input:focus, button:focus {
+    outline: none;
+}
+</style>
